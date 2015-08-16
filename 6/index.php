3569f428b80e0697af9cf0979bfe0559858fb377
wfmaster <miilleramaral@gmail.com>
2015-08-15 22:27:35 -0300

    6 e 7

---
+++ 6/index.php
@@ -0,0 +1,9 @@
+<?php
+
+$nomes = array("Pedro", "Wesley", "teste", "teste2");
+
+for ($i = 0; $i < count($nomes); $i++)
+    echo $nomes[$i] ."<br/>";
+
+
+?>
---
+++ 7/index.php
@@ -0,0 +1,26 @@
+<?php
+
+//Primeira versão da nossa função
+//function soma(){
+//    echo 5 + 5;
+//}
+
+//Nossa função um pouco melhorada
+//function soma()
+//{
+//    $x = 5;
+//    $y = 6;
+//    echo $x + $y;
+//}
+//
+//function soma($x,$y){
+//    echo $x + $y;
+//}
+
+function  media($p1,$p2,$p3,$p4){
+    $resultado = ($p1 + $p2 + $p3 + $p4) / 4;
+    echo $resultado;
+}
+
+media(10,4,5,8);
+
